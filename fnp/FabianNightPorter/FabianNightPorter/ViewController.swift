d58e4d0dde779e9e9519dbaca3c53c550611dfcf
fabianwilliams <fabian@adotob.com>
2017-11-24 20:41:58 -0500

    Basics done in Xcode using SWIFT

---
+++ fnp/FabianNightPorter/FabianNightPorter/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  FabianNightPorter
+//
+//  Created by Fabian Williams on 11/24/17.
+//  Copyright © 2017 Fabian G. Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func changeBackground(_ sender: Any) {
+        view.backgroundColor = UIColor.darkGray
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
