78a14262ecd327097bbae03ce54129b72c3daa5f
fabianwilliams <fabian@adotob.com>
2017-11-25 23:30:12 -0500

    Adding a project that does JSON call from remote REST Endpoint

---
+++ mcj/mycountriesjson/mycountriesjson/TableViewController.swift
@@ -0,0 +1,133 @@
+//
+//  TableViewController.swift
+//  mycountriesjson
+//
+//  Created by Fabian Williams on 11/25/17.
+//  Copyright © 2017 Fabian G. Williams. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var TableData:Array< String > = Array < String >()
+    
+    func do_table_refresh()
+    {
+        self.tableView.reloadData()
+        
+    }
+    
+    func extract_json(_ data: Data)
+    {
+        
+        
+        let json: Any?
+        
+        do
+        {
+            json = try JSONSerialization.jsonObject(with: data, options: [])
+        }
+        catch
+        {
+            return
+        }
+        
+        guard let data_list = json as? NSArray else
+        {
+            return
+        }
+        
+        
+        if let countries_list = json as? NSArray
+        {
+            for i in 0 ..< data_list.count
+            {
+                if let country_obj = countries_list[i] as? NSDictionary
+                {
+                    if let country_name = country_obj["country"] as? String
+                    {
+                        if let country_code = country_obj["code"] as? String
+                        {
+                            TableData.append(country_name + " [" + country_code + "]")
+                        }
+                    }
+                }
+            }
+        }
+        
+        
+        
+        DispatchQueue.main.async(execute: {self.do_table_refresh()})
+        
+    }
+    
+    func get_data_from_url(_ link:String)
+    {
+        let url:URL = URL(string: link)!
+        let session = URLSession.shared
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "GET"
+        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
+        
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (
+            data, response, error) in
+            
+            guard let _:Data = data, let _:URLResponse = response  , error == nil else {
+                
+                return
+            }
+            
+            
+            self.extract_json(data!)
+            
+            
+        })
+        
+        task.resume()
+        
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        get_data_from_url("http://www.kaleidosblog.com/tutorial/tutorial.json")
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return TableData.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = TableData[indexPath.row]
+        
+        return cell
+    }
+
+}
